5bcf2a39953033f720d67ca51c7369be3bb97295
LinJ <405376689@qq.com>
2014-12-30 14:21:21 +0800

    test

--- Camera/src/com/example/camera/MainActivity.java
+++ Camera/src/com/example/camera/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.camera;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,7 +11,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+	    Log.i("", "msg");
 	}
 
 	@Override
